122ff5a505e4bd07aa59c5b2dbd6bc37b2365261
Erfan Sayyari <esayyari@eng.ucsd.edu>
2016-06-13 02:57:03 -0700

    going back to the original version f 1kp

--- mask-for-gt.sh
+++ mask-for-gt.sh
@@ -2,18 +2,17 @@
 
 algfn=$4
 f=genes/$1/$algfn
-#f=$1
 percent=$2
 taxapercent=$3
 out=$f.mask${percent}sites.mask${taxapercent}taxa.fasta
 
-test $# == 3 || { echo  USAGE: gene site_percent taxa_percent ; exit 1;  }
+test $# == 4 || { echo  USAGE: gene site_percent taxa_percent file_name; exit 1;  }
 
 m=`echo $( grep ">" $f|wc -l ) \* $percent / 100 |bc`
-$WS_HOME/pasta/pasta/run_seqtools.py -infile $f -masksites $m -outfile $f.mask${percent}sites.fasta
+run_seqtools.py -infile $f -masksites $m -outfile $f.mask${percent}sites.fasta
 echo From `simplifyfasta.sh $f|wc -L` sites in $f to `wc -L $f.mask${percent}sites.fasta` sites
 
 m2=`echo $( cat $f.mask${percent}sites.fasta|wc -L ) \* $taxapercent / 100 |bc`
-$WS_HOME/pasta/pasta/run_seqtools.py -infile $f.mask${percent}sites.fasta -filterfragments $m2 -outfile $out
+run_seqtools.py -infile $f.mask${percent}sites.fasta -filterfragments $m2 -outfile $out
 
 echo went from `grep ">" $f|wc -l` to `grep ">" $out|wc -l` sequences
