170961027060fa479ef4930070448c3018cec209
Erfan Sayyari <esayyari@eng.ucsd.edu>
2016-10-13 15:15:36 -0700

    adding new bash files, bulk change

---
+++ generate_fragmentary_stat_NA.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#for z in `find /home/erfan/Documents/Research//oasis/data/Insect/Supplementary_Archive_7/nt_masked/genes/filtered/ALICUT_EOG* -name "*.linsi.nt.fas"`; do g=$(dirname $z); for x in `cat $z`; do y=$(echo $x | grep ">" ); if [ "$y" == "" ]; then echo -n $x | awk -vFS="" '{for(i=1;i<=NF;i++)w[$i]++ sum++}END{printf "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s","SEQUENCE","N","-","A","C","G","T","a","c","t","g","ACGT-","SUM"}END{print ""}END{printf "%s,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f","##",w["N"]/sum,w["-"]/sum,w["A"]/sum,w["C"]/sum,w["G"]/sum,w["T"]/sum,w["a"]/sum,w["c"]/sum,w["t"]/sum,w["g"]/sum,(w["A"]+w["T"]+w["C"]+w["-"]+w["G"])/sum,sum}' | sed -e 's/,$//'; echo; else echo $x; fi; done  > $g/fragmentary.stat;echo "working on $g has been finishe"; done
+z=$1
+g=$(dirname $z);
+base=$(basename $z | sed -e "s/.fasta//"); 
+for x in `cat $z`; do 
+	y=$(echo $x | grep ">" ); 
+	if [ "$y" == "" ]; then 
+		echo -n $x | awk -vFS="" '{for(i=1;i<=NF;i++)w[$i]++ sum++}END{printf "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s","SEQUENCE","N","-","A","C","G","T","a","c","t","g","ACGT-","SUM"}END{print ""}END{printf "%s,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f","##",w["N"]/sum,w["-"]/sum,w["A"]/sum,w["C"]/sum,w["G"]/sum,w["T"]/sum,w["a"]/sum,w["c"]/sum,w["t"]/sum,w["g"]/sum,(w["A"]+w["T"]+w["C"]+w["-"]+w["G"])/sum,sum}' | sed -e "s/,$//"; echo; 
+	else 
+		echo $x; 
+	fi; 
+done  > $g/$base.fragmentary.stat;
+echo "working on $base has been finishe"; 
+
+x=$g/$base.fragmentary.stat;
+b=$(basename $x | sed -e 's/.fragmentary.stat//')
+d=$(dirname $x); 
+cat $x | sed -e 's/>\(.*\)/>\1\#/' | sed -e 's/\(.*\)SUM/\&\1SUM\&/' | tr "\n" "@"  | sed -e 's/>/\n/g' | sed -e 's/@//g' | sed -e 's/#\&.*\&##//g' > $d/$b.fragmentary_final.stat; 
+echo "working on $x has been finished"; 
+
+
+
+#for z in `find /home/erfan/Documents/Research//oasis/data/Insect/Supplementary_Archive_7/nt_masked/genes/filtered/ALICUT_EOG* -name "*.linsi.nt.fas"`; do g=$(dirname $z); for x in `cat $z`; do y=$(echo $x | grep ">" ); if [ "$y" == "" ]; then echo -n $x | awk -vFS="" '{for(i=1;i<=NF;i++)w[$i]++ sum++}END{printf "%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s, %s, %s, %s, %s" ,"N","-","A","C","G","T","a","c","t","g","ACGT-","SUM"}END{printf "%f ,%f ,%f ,%f ,%f ,%f ,%f ,%f ,%f ,%f ,%f ,%f ,%f ,%f  ",w["N"]/sum,w["-"]/sum,w["A"]/sum,w["C"]/sum,w["G"]/sum,w["T"]/sum,w["a"]/sum,w["c"]/sum,w["t"]/sum,w["g"]/sum,(w["A"]+w["T"]+w["C"]+w["-"]+w["G"])/sum,sum}' | sed -e 's/,$//'; echo; else echo $x; fi; done > $g/fragmentary.stat;echo "working on $g has been finishe"; done
