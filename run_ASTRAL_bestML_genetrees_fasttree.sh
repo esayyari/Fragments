5fbc84233496e52f7cf76154a5571b1237f9d6a7
Erfan Sayyari <esayyari@eng.ucsd.edu>
2016-10-21 14:10:43 -0700

    adding generateMLBSreplicates.sh

--- run_ASTRAL_bestML_genetrees_fasttree.sh
+++ run_ASTRAL_bestML_genetrees_fasttree.sh
@@ -12,6 +12,7 @@ mkdir -p $work_dir
 if [ -s $DT-runASTRAL.jobs ]; then
 	rm $DT-runASTRAL.jobs
 fi
+if [ -s $suffix ]; then
 while read y; do
 	while read x; do
 		while read h; do
@@ -23,3 +24,15 @@ while read y; do
 		done < $suffix
 	done < $taxa
 done < $site
+else
+while read y; do
+        while read x; do
+			h=""
+                        mkdir -p $work_dir/$DT-mask"$y"sites.mask"$x"taxa$h
+                        cat $path/*/$DT*mask"$y"sites.mask"$x"taxa$h/$DT*mask"$y"sites.mask"$x"taxa$h/fasttree.tre.best.addPoly.rooted.final > $work_dir/$DT-mask"$y"sites.mask"$x"taxa$h/$DT-mask"$y"sites.mask"$x"taxa$h.gene_trees.trees
+                        sed -i 's/e_\([0-9]\)/e-\1/g' $work_dir/$DT-mask"$y"sites.mask"$x"taxa$h/$DT-mask"$y"sites.mask"$x"taxa$h.gene_trees.trees
+                        z=$work_dir/$DT-mask"$y"sites.mask"$x"taxa$h/$DT-mask"$y"sites.mask"$x"taxa$h.gene_trees.trees
+                        printf "java -jar $WS_HOME/ASTRAL/$version -i $z -o $work_dir/$DT-mask"$y"sites.mask"$x"taxa$h/$DT-mask"$y"sites.mask"$x"taxa$h.species_tree.trees > $work_dir/$DT-mask"$y"sites.mask"$x"taxa$h/$DT-mask"$y"sitesx.mask"$x"taxa$h.species_tree.trees.log 2>&1 \n" >> $DT-runASTRAL.jobs
+        done < $taxa
+done < $site
+fi
