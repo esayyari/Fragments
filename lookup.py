d927174ad6d1424d865542167a773c0a94c651c8
esayyari <esayyari@eng.ucsd.edu>
2016-11-21 00:54:42 -0800

    adding lookup.py

---
+++ lookup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+
+import pandas as pd
+
+filename = 'fragmentary_augmented.csv'
+table = pd.read_csv(filename)
+
+filename = 'a'
+#REP GENE BTAXA BLEN STAXA SLEN GENEID FRAG
+sample = pd.read_csv(filename)
+print sample.columns
+sample['REP']
+for i in range(1,51):
+	taxa = sample[sample['REP'] == i]['BTAXA'].values[:]
+	gene = sample[sample['REP'] == i]['GENEID'].values[:]
+	for j in range(0,len(taxa)):
+		print table[(table['ID'] == gene[j])][taxa[j]].values[0] - sample[sample['REP'] == i]['FRAG'].values[j]
+
+
