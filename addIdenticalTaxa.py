347af081d79dd1b080545993fda13c441c1dfa07
Erfan Sayyari <esayyari@eng.ucsd.edu>
2017-07-28 19:23:51 -0700

    bulk change

--- addIdenticalTaxa.py
+++ addIdenticalTaxa.py
@@ -11,14 +11,14 @@ g = open(lstf,'r')
 taxa_list = list(list())
 for lt in g:
 	lt = lt.replace("\n","")
-	lt = lt.replace("_","-")
+#	lt = lt.replace("_","-")
 	taxa = lt.split(" ")
 	taxa_list.append(taxa)
 trees = dendropy.TreeList()
 for line in f:
 	dictExistNode = dict()
 	line = line.replace("\n","")
-	line = line.replace("_","-")
+#	line = line.replace("_","-")
 	tre = dendropy.Tree.get_from_string(line,"newick")
 	for taxa in taxa_list:
 		tmp = list()
@@ -32,14 +32,27 @@ for line in f:
 		dictExistNode[Nd] = tmp
 
 	for Nd in dictExistNode:
+		newP = dendropy.Node()
+		newP = Nd.parent_node.insert_child(0,newP)
+		bl = Nd.edge.length
+		tlabel = Nd.label
+		#Ndlabel = Nd.taxon.label
+		p = Nd.parent_node
+		Nd = p.remove_child(Nd)
+		#tre.prune_taxa(Nd.taxon, update_bipartitions=False, suppress_unifurcations=False)
+		#Nd = dendropy.Node(taxon=tx)
+		newP.insert_child(0,Nd)
+		#dictExistNode[Nd].append(tlabel)
+		c=1
 		for t in dictExistNode[Nd]:
 			taxon_1 = dendropy.Taxon(label=t)
 			if taxon_1 not in tre.taxon_namespace:
 				tre.taxon_namespace.add_taxon(taxon_1)
 			# Create a new node and assign a taxon OBJECT to it (not a label)
 			n = dendropy.Node(taxon=taxon_1, label=t)
-			n.edge.length = Nd.edge.length 
-			Nd.parent_node.insert_child(0, n)
+			n.edge.length = bl
+			newP.insert_child(c, n)
+			c = c + 1
 	trees.append(tre)
 trees.write(
     path=outf,
--- listRemovedTaxa.py
+++ listRemovedTaxa.py
@@ -8,12 +8,13 @@ out = sys.argv[2]
 f=open(filename,'r')
 dictTaxa = dict()
 dictGene = dict()
-for line in f:
+allLines = f.readlines()
+for line in allLines[1:]:
 	line=line.replace("\n","")
 	spLine = line.split(" ")
-	if spLine[0].isdigit():
-		continue
-	dictTaxa[spLine[0]] = spLine[1]
+	#if spLine[1].isdigit():
+	#	continue
+	dictTaxa[str(spLine[0])] = spLine[1]
 f.close()
 gene = list()
 listKey = sorted(dictTaxa.keys())
@@ -21,18 +22,14 @@ for i in range(0,len(listKey)):
 	key = listKey[i]
 	tmp = map(None,dictTaxa[key])
 	gene.append(tmp)
-#gene = zip(*gene)
-#idx= list()
-#for i in range(0,len(gene)):
-#	if set(gene[i]) == {"-"}:
-#		print "found all -"
-#		idx.append(i)
-#gene = [i for j, i in enumerate(gene) if j not in idx]
-#gene = zip(*gene)
 dictToRem = dict()
+
+
+
 for i in range(0,len(gene)):
 	key = listKey[i]
 	g = "".join(gene[i])
+	g=g.strip()
 	if g in dictGene:
 		if dictGene[g] in dictToRem:
 			dictToRem[dictGene[g]].append(key)
@@ -42,8 +39,15 @@ for i in range(0,len(gene)):
 	else:
 		dictGene[g] = key
 o = open(out,'w')
+
+
+
 for key in dictToRem:
-	print >> o,key,
-	for v in dictToRem[key]:
-		print >> o, v,
-print >> o
+#	print >> o,key,
+#	print "here"
+	string = key +" " + " ".join(dictToRem[key])
+	print >> o, string
+#	for v in dictToRem[key]:
+#		print >> o, v,
+#		print v
+#	print >> o
